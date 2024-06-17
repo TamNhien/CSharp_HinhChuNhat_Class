ad9184cd75997dc5a0e151c45be34056595b9774
TamNhien <canhthien91@gmail.com>
2024-06-17 14:12:37 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_HinhChuNhat_Class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Opacity = 0.8;
+        }
+
+        private void btnDientich_Click(object sender, EventArgs e)
+        {
+            HinhChuNhat hcn = new HinhChuNhat();
+            hcn.chieuCao = double.Parse(txtCao.Text);
+            hcn.chieuRong = double.Parse(txtRong.Text);
+
+            txtKQ.Text = hcn.TinhDienTich().ToString(); 
+        }
+
+        private void btnChuVi_Click(object sender, EventArgs e)
+        {
+            HinhChuNhat hcn = new HinhChuNhat();
+            hcn.chieuCao = double.Parse(txtCao.Text);
+            hcn.chieuRong = double.Parse(txtRong.Text);
+
+            txtKQ.Text = hcn.TinhChuVi().ToString();
+        }
+    }
+}
---
+++ HinhChuNhat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_HinhChuNhat_Class
+{
+    internal class HinhChuNhat
+    {
+        public double chieuCao;
+        public double chieuRong;
+
+        public double TinhDienTich()
+        {
+            return chieuCao * chieuRong;
+        }
+        
+        public double TinhChuVi()
+        {
+            return (chieuRong + chieuCao) * 2;
+        }
+    }
+}
